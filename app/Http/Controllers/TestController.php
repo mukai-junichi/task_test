8bff687bb8c5cdda7be7e99ab120cb2bbf5b74dc
mukai-junichi <mju.6000@gmail.com>
2022-03-11 14:22:08 +0900

    クエリビルダを使ってDBの内容を取得する

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Test;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     public function index()
     {
         $values = Test::all();
-        // dd($values);
+
+        $tests = DB::table('tests')
+                    ->select('id')
+                    ->get();
+        dd($tests);
 
         return view('tests.test', compact('values'));
     }
