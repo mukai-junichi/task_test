fce7574d80f8fbe2ffbc47632e910274e4116473
mukai-junichi <mju.6000@gmail.com>
2022-03-15 15:35:45 +0900

    ダミーデータ（シーダ）

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            [
+                'name' => 'あああ',
+                'email' => 'test1@gmail.com',
+                'password' => Hash::make('password123'),
+            ],
+            [
+                'name' => 'いいい',
+                'email' => 'test2@gmail.com',
+                'password' => Hash::make('password123'),
+            ],
+            [
+                'name' => 'ううう',
+                'email' => 'test3@gmail.com',
+                'password' => Hash::make('password123'),
+            ]
+        ]);
+    }
+}
